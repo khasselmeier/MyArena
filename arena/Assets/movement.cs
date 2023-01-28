2f021d1d51badb33b038029e09a48126439febb4
khasselmeier <123027444+khasselmeier@users.noreply.github.com>
2023-01-28 12:24:54 -0700

    added front/back and left/right movements

---
+++ arena/Assets/movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * 0.025f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * 0.025f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * 0.025f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * 0.025f;
+        }
+    }
+}
