443ae18b939ff2e525c8ef9db066bb85d71079dc
khasselmeier <123027444+khasselmeier@users.noreply.github.com>
2023-01-28 12:32:15 -0700

    changed movement

--- arena/Assets/movement.cs
+++ arena/Assets/movement.cs
@@ -13,22 +13,22 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.S))
         {
             transform.position += Vector3.forward * 0.025f;
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.W))
         {
             transform.position += Vector3.back * 0.025f;
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.D))
         {
             transform.position += Vector3.left * 0.025f;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.position += Vector3.right * 0.025f;
         }
