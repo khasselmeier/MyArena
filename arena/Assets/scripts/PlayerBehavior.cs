ccbf21c2857fc9d5ef1726d730ab1c89732ab352
khasselmeier <123027444+khasselmeier@users.noreply.github.com>
2023-02-16 00:14:32 -0700

    added ai movements

--- arena/Assets/scripts/PlayerBehavior.cs
+++ arena/Assets/scripts/PlayerBehavior.cs
@@ -1,20 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Reflection;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
     public float moveSpeed = 10f;
     public float rotateSpeed = 75f;
-
     public float jumpVelocity = 5f;
-    public float distanceToGround = .1f;
+    public float distanceToGround = 0.1f;
     public LayerMask groundLayer;
-
     public GameObject bullet;
     public float bulletSpeed = 100f;
+    public bool isGrounded = true;
 
     private float vInput;
     private float hInput;
@@ -34,37 +32,47 @@ public class PlayerBehavior : MonoBehaviour
         vInput = Input.GetAxis("Vertical") * moveSpeed;
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
 
-        /*
-        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
-        this.transform.Translate(Vector3.up * hInput * Time.deltaTime);
-        */
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            _rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Ground")
+        {
+            isGrounded = true;
+        }
     }
 
     void FixedUpdate()
     {
+        /*
         if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
         {
             _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
         }
+        */
 
         Vector3 rotation = Vector3.up * hInput;
         Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
         _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
         _rb.MoveRotation(_rb.rotation * angleRot);
-
-        if (Input.GetMouseButtonDown(0))
+       
+        if(Input.GetMouseButtonDown(0))
         {
             GameObject newBullet = Instantiate(bullet,
                 this.transform.position + new Vector3(1, 0, 0),
                     this.transform.rotation) as GameObject;
 
             Rigidbody bulletRB = newBullet.GetComponent<Rigidbody>();
-
             bulletRB.velocity = this.transform.forward * bulletSpeed;
         }
     }
 
-    private void OnCollisionEnter(Collision hit)
+    private void OnCollision(Collision hit)
     {
         switch (hit.gameObject.tag)
         {
